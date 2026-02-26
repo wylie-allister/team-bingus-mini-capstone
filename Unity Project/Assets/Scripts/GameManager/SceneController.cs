877bd2c003ddef8fa6d2977d56e154e576fdd1f8
000954560@mohawkcollege.ca <brandon.callaway.29@gmail.com>
2026-02-25 22:01:12 -0500

    Create scene controller script | to be appended to the start screen

---
+++ Unity Project/Assets/Scripts/GameManager/SceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+    
+    private int currentSceneIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void GoToNextScene()
+    {
+        currentSceneIndex++;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
+    public void GoToScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void GoToStart()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+}

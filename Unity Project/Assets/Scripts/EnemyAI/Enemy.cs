e9c51697aebfb85ef3ed071edde62afc30020fc3
000954560@mohawkcollege.ca <brandon.callaway.29@gmail.com>
2026-02-25 20:54:43 -0500

    Update enemy script to react to thrown objects Create Enemy sandbox scene Create throwable object distract sphere with unique tag

--- Unity Project/Assets/Scripts/EnemyAI/Enemy.cs
+++ Unity Project/Assets/Scripts/EnemyAI/Enemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEngine.AI; // Required for NavMesh classes
+using UnityEngine.AI;
+using UnityEngine.TestTools; // Required for NavMesh classes
 
 public class Enemy : MonoBehaviour
 {
@@ -7,6 +8,15 @@ public class Enemy : MonoBehaviour
 
     private Vector3 spawnPoint;
     private Vector3 guardPoint;
+    public Vector3 finalDistractionPoint;
+
+    private bool isIdle = false;
+    public Vector3 distractionSourcePosition;
+
+    public bool isDistracted = false;
+    private bool obtainNewDistractPoint = true;
+
+    public GameObject exclaimMarkerObject;
 
     void Start()
     {
@@ -21,14 +31,18 @@ public class Enemy : MonoBehaviour
         
         // Set nav agent destination to guardpoint
         agent.SetDestination(guardPoint);
+        exclaimMarkerObject.SetActive(false);
+        
     }
 
     
     void Update()
     {
+        exclaimMarkerObject.SetActive(isDistracted);
+        
         RaycastHit hit;
         
-        // Cast ray from base of object upwards, if an item is present, move guardpoint
+        // Cast ray from base of object upwards, if an item/collider/object/model is present, move guardpoint
         if (Physics.SphereCast(guardPoint, 2.0f, Vector3.up, out hit, 3.0f))
         {
             if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Ground"))
@@ -36,16 +50,99 @@ public class Enemy : MonoBehaviour
                 guardPoint += new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
             }
         }
+
+        HandleGuarding();
+        HandleDistraction();
+
+    }
+
+    void HandleDistraction()
+    {
+        if (!isDistracted)
+            return;
+
+        isIdle = false;
+        
+        if (distractionSourcePosition != null && agent.destination != finalDistractionPoint && obtainNewDistractPoint)
+        {
+            finalDistractionPoint =  GetDirectionXZ(distractionSourcePosition, true);
+            finalDistractionPoint = Vector3.Normalize(finalDistractionPoint) * 15 ;
+            finalDistractionPoint += this.transform.position;
+            
+            
+            agent.SetDestination(finalDistractionPoint);
+            obtainNewDistractPoint = false;
+        }
+
+        if (IsAtPointWithinRange(finalDistractionPoint, 2.0f))
+        {
+            isDistracted = false;
+            obtainNewDistractPoint = true;
+            agent.SetDestination(spawnPoint);
+        }
+        
+    }
+
+    // Guards between enemy spawn point and a given guard point
+    void HandleGuarding()
+    {
+        if (isIdle || isDistracted)
+        {
+            return;
+        }
         
         // Set navagent dest to guard point if at spawn point
-        if (this.transform.position.x == spawnPoint.x && this.transform.position.z == spawnPoint.z)
+        if (IsAtPointWithinRange(spawnPoint, 1f))
         {
             agent.SetDestination(guardPoint);
         }
         // Set navagent dest to spawn point if at guard point
-        else if (this.transform.position.x == guardPoint.x && this.transform.position.z == guardPoint.z)
+        else if (IsAtPointWithinRange(guardPoint, 1f))
         {
             agent.SetDestination(spawnPoint);
+        }
+    }
+
+    bool isAtPoint(Vector3 point)
+    {
+        if (this.transform.position.x == point.x && this.transform.position.z == point.z)
+            return true;
+        
+        return false;
+    }
+
+    bool IsAtPointWithinRange(Vector3 point, float range)
+    {
+        if (this.transform.position.x >= point.x - range && this.transform.position.x <= point.x + range)
+        {
+            if (this.transform.position.z >= point.z - range && this.transform.position.z <= point.z + range)
+                return true;
+        }
+
+        return false;
+    }
+
+    Vector3 GetDirectionXZ(Vector3 point, bool swapDir = false)
+    {
+        if (swapDir)
+        {
+            return new Vector3( this.transform.position.x - point.x, 0,
+                this.transform.position.z - point.z);
+        }
+        else
+        {
+            return new Vector3(point.x - this.transform.position.x, 0,
+                point.z - this.transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("DistractionPoint"))
+        {
+            Debug.Log("DISTRACTED");
+            distractionSourcePosition = collision.gameObject.transform.position;
+            isDistracted = true;
         }
     }
 }
